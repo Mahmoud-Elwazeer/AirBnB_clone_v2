d49aaa70399f1d7d103c820f5250535bd9945d8b
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-09-19 21:46:57 +0300

    sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+--  prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' 
+IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES 
+ON `hbnb_dev_db`.*
+TO 'hbnb_dev'@'localhost';
+
