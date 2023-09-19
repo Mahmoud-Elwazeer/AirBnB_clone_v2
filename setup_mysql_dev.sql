7a5cd413ab34cfc0c7475db6fb1e0cf78ceb60cf
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-09-19 11:54:17 -0700

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,11 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' 
-IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
+IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT SELECT 
+ON `performance_schema`.* 
+TO 'hbnb_dev'@'localhost';
 
 GRANT ALL PRIVILEGES 
 ON `hbnb_dev_db`.*
