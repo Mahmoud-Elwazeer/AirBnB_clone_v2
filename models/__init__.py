d6220d047bab65a61d7f7f7cf3309e7dc7f3dd2c
Karim-Moharm <karimmoharm222@gmail.com>
2023-09-21 02:30:11 +0300

    update init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,30 @@
+# #!/usr/bin/python3
+# """the __init__ module
+# """
+# from models.engine.file_storage import FileStorage
+# from models.engine.db_storage import DBStorage
+# import os
+
+# storage_type = os.getenv("HBNB_TYPE_STORAGE")
+# if (storage_type == "db"):
+#     storage = DBStorage()
+#     storage.reload()
+# else:
+#     storage = FileStorage()
+#     storage.reload()
+
+
 #!/usr/bin/python3
 """the __init__ module
 """
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+
 import os
 
-storage_type = os.getenv("HBNB_TYPE_STORAGE")
-if (storage_type == "db"):
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
+
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
