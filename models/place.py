baacd6bc74ac3552fb537ad86c5e78e5788a5852
Karim-Moharm <karimmoharm222@gmail.com>
2023-09-22 01:01:43 +0300

    fix2 place

--- models/place.py
+++ models/place.py
@@ -12,9 +12,9 @@ if (models.storage_type == "db"):
     place_amenity = Table(
         'place_amenity', Base.metadata,
         Column("place_id", String(60), ForeignKey("places.id"),
-            pimary_key=True, nullable=False),
+               primary_key=True, nullable=False),
         Column("amenity_id", String(60), ForeignKey("amenities.id"),
-            primary_key=True, nullable=False)
+               primary_key=True, nullable=False)
     )
 
 
@@ -29,7 +29,7 @@ class Place(BaseModel, Base):
         user_id = Column(String(60), ForeignKey(
             "users.id"), nullable=False)
         name = Column(String(120), nullable=False)
-        description = Column(String(1024), nullable=False)
+        description = Column(String(1024))
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
@@ -38,7 +38,7 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
 
         reviews = relationship("Review", backref="place",
-                            cascade="all, delete, save-update")
+                               cascade="all, delete, save-update")
         amenities = relationship(
             "Amenity", secondary=place_amenity, viewonly=False)
 
@@ -72,3 +72,22 @@ class Place(BaseModel, Base):
                 if self.id == Review.state_id:
                     lst.append(review)
             return lst
+
+        @property
+        def amenities(self):
+            """returns the list of Amenity instances based on
+            the attribute amenity_ids
+            """
+            from models.amenity import Amenity
+            from models import storage
+            lst = []
+            for amenity in list(storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    lst.append(amenity)
+            return lst
+
+        @amenities.setter
+        def amenities(self, value):
+            from models.amenity import Amenity
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
