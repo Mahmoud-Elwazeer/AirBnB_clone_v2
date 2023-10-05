040a35c485f82cda5ee90f6df29000545f18bc4e
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-05 20:59:06 +0300

    web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""import libraries"""
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+    """generate tgz"""
+    try:
+        local("mkdir -p versions")
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        arch_name = f"web_static_{time}.tgz"
+        local(f"tar -cvzf versions/{arch_name} web_static")
+        return (f"versions/{arch_name}")
+    except Exception:
+        return None
