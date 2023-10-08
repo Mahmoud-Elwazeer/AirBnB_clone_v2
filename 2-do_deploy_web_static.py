8b85c68cd8176fc948946a22672c75fbff96f2f5
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-08 15:33:50 +0000

    add sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,12 +26,12 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put("{}".format(archive_path), "/tmp/")
-        run("mkdir -p {}{}".format(path, no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file_n, path, no_ext))
-        run("rm /tmp/{}".format(file_n))
-        run("mv {}{}/web_static/* {}{}/".format(path, no_ext, path, no_ext))
-        run("rm -rf {}{}/web_static".format(path, no_ext))
-        run("rm -rf /data/web_static/current")
+        run("sudo mkdir -p {}{}".format(path, no_ext))
+        run("sudo tar -xzf /tmp/{} -C {}{}/".format(file_n, path, no_ext))
+        run("sudo rm /tmp/{}".format(file_n))
+        run("sudo mv {}{}/web_static/* {}{}/".format(path, no_ext, path, no_ext))
+        run("sudo rm -rf {}{}/web_static".format(path, no_ext))
+        run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {}{}/ /data/web_static/current".format(path, no_ext))
         return True
     except Exception:
