1d8c3bdcc2492d715976f0ff042224bf2e46b219
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-21 17:15:12 +0300

    fix style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,12 @@ from flask import Flask
 
 hbnb = Flask(__name__)
 
+
 @hbnb.route("/", strict_slashes=False)
 def homepage():
+    """Home page"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     hbnb.run(debug=True, host="0.0.0.0")
