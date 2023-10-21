4f4a259ac9fcf319355b2bdc5c7ec8129f13b6b0
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-21 17:12:03 +0300

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""import libraries"""
+from flask import Flask
+
+hbnb = Flask(__name__)
+
+@hbnb.route("/", strict_slashes=False)
+def homepage():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    hbnb.run(debug=True, host="0.0.0.0")
