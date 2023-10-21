73624affae919f18f37b50173c3dc2e3943f3edc
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-21 21:19:57 +0300

    fix add in file storage change to dict

--- models/base_model.py
+++ models/base_model.py
@@ -69,14 +69,24 @@ class BaseModel:
         """returns a dictionary containing all keys/values
         of the instance
         """
-        class_name = self.__class__.__name__
-        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.__dict__["__class__"] = class_name
+        time = "%Y-%m-%dT%H:%M:%S.%f"
+        # class_name = self.__class__.__name__
+        # self.created_at = self.created_at.strftime(time)
+        # self.updated_at = self.updated_at.strftime(time)
+        # self.__dict__["__class__"] = class_name
 
-        if "_sa_instance_state" in self.__dict__.keys():
-            del self.__dict__["_sa_instance_state"]
-        return self.__dict__
+        # if "_sa_instance_state" in self.__dict__.keys():
+        #     del self.__dict__["_sa_instance_state"]
+        # return self.__dict__
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
 
     def delete(self):
         """delete the current instance from the storage
